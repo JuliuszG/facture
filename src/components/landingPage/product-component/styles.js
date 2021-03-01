1fcce501604b536325f1ed56121f47ccca747820
JuliuszG <juliusz.goral@gmail.com>
2021-03-01 22:43:35 +0100

    dodalem komponent z produktami do landing page

---
+++ src/components/landingPage/product-component/styles.js
@@ -0,0 +1,77 @@
+import styled from 'styled-components'
+import Image from 'gatsby-image'
+import { motion } from 'framer-motion'
+
+export const ProductComponentWrapper = styled.div`
+    padding: ${ ({ alignment }) => alignment === 'bottom' ? '3%' : 0 };
+    position: relative;
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    height: ${ ({ height }) => height === 'small' ? '60vh' : '100vh' };
+    align-items: ${ ({ alignment }) => alignment === 'bottom' ? 'flex-end' : 'center' };
+    font-size: 16px;
+    text-align: center;
+    overflow: hidden;
+    .content {
+        z-index: 10;
+    }
+    .small, p {
+        font-size: 0.875em;
+        font-family: "Poppins", sans-serif;
+        color: ${ ({ themeColor }) => themeColor === 'light' ? '#fff' : '#191919' };
+    }
+    .small {
+        font-weight: 300;
+        text-transform: uppercase;
+        line-height: 150%;
+    }
+    p {
+        font-weight: 400;
+        line-height: 178.57%;
+        margin-bottom: 40px;
+    }
+    h2 {
+        font-size: 3.75em;
+        line-height: 120%;
+        font-family: "Butler", serif;
+        font-weight: 300;
+        color: ${ ({ themeColor }) => themeColor === 'light' ? '#fff' : '#191919' };
+        margin: 23px 0 10px;
+    }
+`
+
+export const ProductImage = styled(Image)`
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+`
+
+export const ButtonLink = styled(motion.div)`
+    border: 1px solid ${ ({ themeColor }) => themeColor === 'light' ? '#fff' : '#191919' };
+    background: ${ ({ themeColor }) => themeColor === 'light' ? '#FFFFFF26' : 'transparent' };
+    padding: 10px 30px 12px;
+    cursor: pointer;
+    transition: all 0.3s ease-in;
+    display: inline-block;
+    a {
+        color: ${ ({ themeColor }) => themeColor === 'light' ? '#fff' : '#191919' };
+        text-decoration: none;
+        font-family: "Poppins", sans-serif;
+        font-weight: 300;
+        font-size: 1em;
+        text-transform: uppercase;
+        width: 100%;
+        height: 100%;
+        z-index: 666;
+        span{
+            margin-left: 32px;
+        }
+    }
+    &:hover {
+        background: ${ ({ themeColor }) => themeColor === 'light' ? '#FFFFFF' : '#191919' };
+        a {
+            color: ${ ({ themeColor }) => themeColor === 'light' ? '#191919' : '#FFFFFF' };
+        }
+    }
+`
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,116 @@
 import React from "react"
+import ProductComponent from "../components/landingPage/product-component/ProductComponent"
 import SEO from "../components/seo"
+import { graphql } from 'gatsby'
 
-const IndexPage = () => (
+export const query = graphql`
+  {
+    section1: file(relativePath: {eq: "banner/banner1.png"}) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    section2: file(relativePath: {eq: "banner/banner2.png"}) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    section3: file(relativePath: {eq: "banner/banner3.png"}) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    section4: file(relativePath: {eq: "banner/banner4.png"}) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
+
+const IndexPage = ({ data }) => (
   <>
     <SEO title="Home" />
-    <p className="no-font">
-    The spectacle before us was indeed sublime.
-    </p>
-    <p className="butt">
-    The spectacle before us was indeed sublime.
-    </p>
-    <p className="butt light">
-    The spectacle before us was indeed sublime.
-    </p>
-    <p className="pop">
-    The spectacle before us was indeed sublime.
-    </p>
+    <ProductComponent 
+      alignment="center" 
+      themeColor="light" 
+      height="small"
+      content={{
+        category: 'sitting',
+        title: 'Ripple Chaise Lounge',
+        paragraph: 'This piece is an imaginative re-interpretation of the classic chaise lounge.'
+      }}
+      image={{
+        src: data.section1.childImageSharp.fluid,
+        alt: "Background Image"
+      }}
+      link={{
+        name: "See Sittings",
+        url: "/products?tag=sittings"
+      }}
+    />
+      <ProductComponent 
+      alignment="bottom" 
+      themeColor="dark" 
+      height="big"
+      content={{
+        category: 'Wall',
+        title: 'Offset Round',
+        paragraph: 'This wall object playfully references the human eye itself.'
+      }}
+      image={{
+        src: data.section2.childImageSharp.fluid,
+        alt: "Background Image"
+      }}
+      link={{
+        name: "See Wall",
+        url: "/products?tag=wall"
+      }}
+    />
+     <ProductComponent 
+      alignment="center" 
+      themeColor="light" 
+      height="big"
+      content={{
+        category: 'Tables',
+        title: 'Ripple Table',
+        paragraph: 'The Ripple Dining Table consists of a circular top set on a tapered conical base'
+      }}
+      image={{
+        src: data.section3.childImageSharp.fluid,
+        alt: "Background Image"
+      }}
+      link={{
+        name: "See Tables",
+        url: "/products?tag=tables"
+      }}
+    />
+    <ProductComponent 
+      alignment="bottom" 
+      themeColor="dark" 
+      height="big"
+      content={{
+        category: 'Objects',
+        title: 'Green Column',
+        paragraph: 'The Green Column consists of a circular top set on a tapered conical base'
+      }}
+      image={{
+        src: data.section4.childImageSharp.fluid,
+        alt: "Background Image"
+      }}
+      link={{
+        name: "See Objects",
+        url: "/products?tag=objects"
+      }}
+    />
   </>
 )
 
